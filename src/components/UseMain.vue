c3a95d3619e742f3eba788acc7a24eba68ac9328
ramyus889 <ramyus889.com>
2024-09-18 12:43:39 +0300

    add

--- src/components/UseMain.vue
+++ src/components/UseMain.vue
@@ -1,32 +1,36 @@
 <script setup></script>
 <template>
   <div class="bg-[#283652] w-full pb-1 flex place-content-center">
-    <div class="mt-[176px] max-w-[1440px] flex place-content-center px-5">
-      <div class="text-center">
-        <div class="flex place-content-center">
-          <img src="/UseBadge.svg" alt="" />
-        </div>
-        <div
-          class="lg:text-[60px] sm:text-[40px] text-[25px] text-white font-bold leading-[60px] mt-[16px]"
-        >
-          Global Payments Switch
-        </div>
-        <div class="lg:text-[60px] sm:text-[40px] text-[25px] text-[#338cff] font-bold">
-          Fast.Reliable.Affordable.
-        </div>
-        <div class="text-[20px] text-white">
-          Full stack payment infrastructure solution to embrace diversity, reduce payment friction
-          and manage compliance
-        </div>
-        <div class="flex sm:flex-row flex-col gap-[36px] place-content-center mt-[44px]">
-          <button
-            class="bg-[#338cff] text-white font-bold text-[18px] px-[24px] py-[8px] rounded-full"
+    <div class="mt-[176px] max-w-[1440px] flex place-content-center">
+      <div class="">
+        <div class="text-center px-5">
+          <div class="flex place-content-center">
+            <img src="/UseBadge.svg" alt="" />
+          </div>
+          <div
+            class="lg:text-[60px] sm:text-[40px] text-[25px] text-white font-bold leading-[60px] mt-[16px]"
           >
-            Get started {{ '>' }}
-          </button>
-          <button class="bg-white text-black font-bold text-[18px] px-[24px] py-[8px] rounded-full">
-            Get in touch {{ '>' }}
-          </button>
+            Global Payments Switch
+          </div>
+          <div class="lg:text-[60px] sm:text-[40px] text-[25px] text-[#338cff] font-bold">
+            Fast.Reliable.Affordable.
+          </div>
+          <div class="text-[20px] text-white">
+            Full stack payment infrastructure solution to embrace diversity, reduce payment friction
+            and manage compliance
+          </div>
+          <div class="flex sm:flex-row flex-col gap-[36px] place-content-center mt-[44px]">
+            <button
+              class="bg-[#338cff] text-white font-bold text-[18px] px-[24px] py-[8px] rounded-full"
+            >
+              Get started {{ '>' }}
+            </button>
+            <button
+              class="bg-white text-black font-bold text-[18px] px-[24px] py-[8px] rounded-full"
+            >
+              Get in touch {{ '>' }}
+            </button>
+          </div>
         </div>
         <div class="flex place-content-center mt-[100px] max-w-[1040px]">
           <video src="/video/heromain.mp4" autoplay muted playsinline class=""></video>
